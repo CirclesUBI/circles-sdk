7d62a79728ffac9df635a33a8e9ce89f78edb6bd
jaensen <4954577+jaensen@users.noreply.github.com>
2024-01-31 13:28:45 +0000

    adapted the generateAbisFromSource.sh script to pull the 1155 based hub

--- packages/abi/generateAbisFromSource.sh
+++ packages/abi/generateAbisFromSource.sh
@@ -5,13 +5,17 @@ mkdir ./.contracts
 
 git clone https://github.com/CirclesUBI/circles-contracts-v2.git ./.contracts
 cd ./.contracts
+git checkout 20240129-erc1155-evaluation
 forge build
 cd ..
 
+rm -rf ./abi
 mkdir ./abi
 cat ./.contracts/out/Graph.sol/Graph.json | jq .abi > ./abi/Graph.json
 cat ./.contracts/out/MintSplitter.sol/MintSplitter.json | jq .abi > ./abi/MintSplitter.json
 cat ./.contracts/out/GroupCircle.sol/GroupCircle.json | jq .abi > ./abi/GroupCircle.json
 cat ./.contracts/out/TimeCircle.sol/TimeCircle.json | jq .abi > ./abi/TimeCircle.json
+cat ./.contracts/out/Hub.sol/Hub.json | jq .abi > ./abi/Hub.json
+cat ./.contracts/out/Hub.sol/WrappedERC20.json | jq .abi > ./abi/WrappedERC20.json
 
 rm -rf ./.contracts
