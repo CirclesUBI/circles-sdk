d28e3de8e20b097b3b30f63224d7b01f87510ca0
jaensen <4954577+jaensen@users.noreply.github.com>
2024-02-01 12:16:44 +0000

    build 'dev' in generateAbisFromSource.sh

--- packages/abi/generateAbisFromSource.sh
+++ packages/abi/generateAbisFromSource.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-rm -rf ./.contracts
-mkdir ./.contracts
+if [ -d "./contracts" ]; then
+  cd ./contracts
+  git pull
+else
+  mkdir ./.contracts
+  git clone https://github.com/CirclesUBI/circles-contracts-v2.git ./.contracts
+  cd ./.contracts
+fi
 
-git clone https://github.com/CirclesUBI/circles-contracts-v2.git ./.contracts
-cd ./.contracts
-git checkout 20240129-erc1155-evaluation
+git checkout dev
 forge build
+
 cd ..
 
 rm -rf ./abi
 mkdir ./abi
-cat ./.contracts/out/Graph.sol/Graph.json | jq .abi > ./abi/Graph.json
-cat ./.contracts/out/MintSplitter.sol/MintSplitter.json | jq .abi > ./abi/MintSplitter.json
-cat ./.contracts/out/GroupCircle.sol/GroupCircle.json | jq .abi > ./abi/GroupCircle.json
-cat ./.contracts/out/TimeCircle.sol/TimeCircle.json | jq .abi > ./abi/TimeCircle.json
+
 cat ./.contracts/out/Hub.sol/Hub.json | jq .abi > ./abi/Hub.json
 cat ./.contracts/out/Hub.sol/WrappedERC20.json | jq .abi > ./abi/WrappedERC20.json
 
