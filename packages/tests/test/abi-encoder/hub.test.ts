bdcc65585f9986653967f254b73e92adc41113dd
jaensen <4954577+jaensen@users.noreply.github.com>
2024-02-02 12:37:13 +0000

    described the tests in the form of states and transitions

--- packages/tests/test/abi-encoder/hub.test.ts
+++ packages/tests/test/abi-encoder/hub.test.ts
@@ -1,33 +1,114 @@
 describe('Hub', () => {
-  describe('registerHuman', () => {
-    it('should register a human', async () => {});
-    it('should revert when trying to register the same human again', async () => {});
 
-    describe('registered humans', () => {
-      it('should have an associated token with their avatar-address as token-id', async () => {});
+  const UNREGISTERED_AVATAR = "UNREGISTERED_AVATAR";
+  const REGISTERED_AVATAR = "REGISTERED_AVATAR";
+  const REGISTERED_HUMAN = "REGISTERED_HUMAN";
+  const REGISTERED_ORGANIZATION = "REGISTERED_ORGANIZATION";
+  const REGISTERED_GROUP = "REGISTERED_GROUP";
+  const REGISTERED_CUSTOM_GROUP = "REGISTERED_CUSTOM_GROUP";
+  const INVITED_HUMAN = "INVITED_HUMAN";
+  const INVITED_PAUSED_HUMAN = "INVITED_PAUSED_HUMAN";
+  const INVITED_ACTIVE_HUMAN = "INVITED_ACTIVE_HUMAN";
+  const V1_ACTIVE = "V1_ACTIVE";
+  const V1_STOPPED = "V1_STOPPED";
+  const V2_PAUSED = "V2_PAUSED";
+  const V2_ACTIVE = "V2_ACTIVE";
+  const V2_STOPPED = "V2_STOPPED";
+  const MINT_PERSONAL_CIRCLES = "MINT_PERSONAL_CIRCLES";
+
+  const personalCirclesToken = "personal Circles token";
+  const msgSender = "msg.sender";
+  const v1 = "v1";
+  const v2 = "v2";
+
+  describe("state transitions", () => {
+    describe("UNREGISTERED_AVATAR to REGISTERED_AVATAR", () => {
+      // A REGISTERED_AVATAR is either one of these:
+      // * REGISTERED_HUMAN | INVITED_HUMAN | INVITED_PAUSED_HUMAN | INVITED_ACTIVE_HUMAN
+      // * REGISTERED_ORGANIZATION
+      // * REGISTERED_GROUP | REGISTERED_CUSTOM_GROUP
+      // Any of these states can be reached only once per msg.sender.
+      it(`can become a ${REGISTERED_HUMAN} (registerHuman)`, async () => {});
+      it(`can become a ${REGISTERED_ORGANIZATION} (registerOrganization)`, async () => {});
+      it(`can become a ${REGISTERED_GROUP} (registerGroup)`, async () => {});
+      it(`can become a ${REGISTERED_CUSTOM_GROUP} (registerCustomGroup)`, async () => {});
+      it(`only one path can be taken per ${msgSender}`, async () => {});
+      it(`only if ${msgSender} is not already registered`, async () => {});
+
+      describe(`${UNREGISTERED_AVATAR} to ${REGISTERED_HUMAN} (ON: registerHuman)`, () => {
+        it("only before REGISTRATION_PERIOD_END", async () => {});
+        it(`only if ${msgSender} has a token at the ${v1} hub`, async () => {});
+        it(`only if ${msgSender}'s ${v1} token is ${V1_STOPPED}`, async () => {});
+        it(`create a ${personalCirclesToken} for ${msgSender}`, async () => {});
+      });
+
+      // TODO: Are organizations or groups migrated from v1 to v2?
+      describe(`${UNREGISTERED_AVATAR} to ${REGISTERED_ORGANIZATION} (ON: registerOrganization)`, () => {
+      });
+
+      describe(`${UNREGISTERED_AVATAR} to ${REGISTERED_GROUP} (ON: registerGroup)`, () => {
+        it(`must have the standard treasury contract`, async () => {});
+      });
+
+      describe(`${UNREGISTERED_AVATAR} to ${REGISTERED_CUSTOM_GROUP} (ON: registerCustomGroup)`, () => {
+        it(`must have a treasury contract`, async () => {});
+        it(`can have the standard treasury contract`, async () => {});
+        it(`can have a custom treasury contract`, async () => {});
+      });
+
+      describe(`${UNREGISTERED_AVATAR} to ${INVITED_HUMAN} (ON: inviteHuman)`, () => {
+        // An invitation is a special case of a registration.
+        // It is executed by an INVITER to register an INVITEE.
+        // The INVITER must be a REGISTERED_HUMAN and the INVITEE must be an UNREGISTERED_AVATAR.
+        // When the INVITEE became an INVITED_HUMAN, the contract mints the INVITEE a welcome bonus consisting of the INVITED_HUMAN's personal Circles.
+        // The INVITER is charged with the invitation fee.
+        // TODO: Any avatar can invite any address (e.g. CREATE2 address with nothing deployed on yet)?.
+        it(`only if INVITER (${msgSender}) is a REGISTERED_HUMAN`, async () => {});
+        it("only if INVITEE is not already a REGISTERED_AVATAR", async () => {});
+        it(`only if INVITER (${msgSender}) has enough ${personalCirclesToken}s to pay the invitation fee`, async () => {});
+        it("mint INVITEE's welcome bonus", async () => {});
+        it(`charge INVITER (${msgSender}) the invitation fee`, async () => {});
+        it(`create a ${personalCirclesToken} for INVITEE`, async () => {});
+
+        describe(`${UNREGISTERED_AVATAR} to ${INVITED_PAUSED_HUMAN} (ON: inviteHuman)`, () => {
+          it(`only if INVITEE's ${v1} token is ${V1_ACTIVE}`, async () => {});
+          it(`set INVITEE's ${personalCirclesToken}'s minting state to ${V2_PAUSED}`, async () => {});
+        });
+
+        describe(`${UNREGISTERED_AVATAR} to ${INVITED_ACTIVE_HUMAN} (ON: inviteHuman)`, () => {
+          it(`only if INVITEE's ${v1} token is ${V1_STOPPED}`, async () => {});
+          it(`set INVITEE's ${personalCirclesToken}'s minting state to ${V2_ACTIVE}`, async () => {});
+        });
+      });
     });
-  });
 
-  describe('registerOrganization', () => {
-    it('should register an organization', async () => {});
-    it('should revert when trying to register the same organization again', async () => {});
-  });
+    describe(`(${REGISTERED_HUMAN} | ${INVITED_HUMAN}) to ${MINT_PERSONAL_CIRCLES} (ON: personalMint)`, () => {
+      describe(`${INVITED_PAUSED_HUMAN} to ${INVITED_ACTIVE_HUMAN} (ON: personalMint)`, () => {
+        it(`only if INVITEE's ${v1} token is ${V1_STOPPED}`, async () => {});
+        it(`only if INVITEE's ${personalCirclesToken}'s minting state is ${V2_PAUSED}`, async () => {});
+        it(`set INVITEE's ${personalCirclesToken}'s minting state to ${V2_ACTIVE}`, async () => {});
+      });
+    });
 
-  describe('registerGroup', () => {
-    it('should register a group', async () => {});
-    it('should revert when trying to register the same group again', async () => {});
-  });
+    describe(`Minting state of ${v1} Circles`, () => {
+      it(`can become ${V1_ACTIVE}`, async () => {});
+      it(`can become ${V1_STOPPED}`, async () => {});
+      it(`can't become ${V1_ACTIVE} again once ${V1_STOPPED}`, async () => {});
+    });
 
-  describe('trust', () => {
-    it('can be established from organization to human', async () => {});
-    it('can be established from human to human', async () => {});
-    it('can be established from group to human', async () => {});
-    it('can be established from human to organization', async () => {});
-    it('can be established from any avatar to arbitrary addresses', async () => {});
+    describe(`Minting state of ${v2} Circles`, () => {
+      it(`can become ${V2_PAUSED}`, async () => {});
+      it(`can become ${V2_ACTIVE}`, async () => {});
+      it(`can become ${V2_STOPPED}`, async () => {});
+      it(`can't become ${V2_PAUSED} again once ${V2_ACTIVE}`, async () => {});
+      it(`can't become ${V2_PAUSED} again once ${V2_STOPPED}`, async () => {});
+      it(`can't become ${V2_ACTIVE} again once ${V2_STOPPED}`, async () => {});
+    });
 
-    describe("established trust", () => {
-      it('can expire', async () => {});
-      it('can be revoked', () => {});
+    describe(`${personalCirclesToken}s can be ${v1} or ${v2} tokens`, () => {
+      it(`a ${V2_PAUSED} token can coexist with an ${V1_ACTIVE} token for the same ${REGISTERED_AVATAR}`, async () => {});
+      it(`an ${V2_ACTIVE} token can coexist with a ${V1_STOPPED} token for a ${REGISTERED_AVATAR}`, async () => {});
+      it(`an ${V2_ACTIVE} token cannot coexist with an ${V1_ACTIVE} token for the same ${REGISTERED_AVATAR}`, async () => {});
     });
   });
 });
