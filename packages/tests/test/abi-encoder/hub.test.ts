7f6d475a8ba7e780bf86905bd829de28ecd4ca2e
Ingo Collatz <incollatz@mail.de>
2024-02-02 16:45:10 +0000

    explicit check for v1 stopped in minting

--- packages/tests/test/abi-encoder/hub.test.ts
+++ packages/tests/test/abi-encoder/hub.test.ts
@@ -63,18 +63,18 @@ describe('Hub', () => {
       // * REGISTERED_ORGANIZATION
       // * REGISTERED_GROUP | REGISTERED_CUSTOM_GROUP
       // Any of these states can be reached only once per msg.sender.
-      it(`can become a ${REGISTERED_HUMAN} (registerHuman)`, async () => {});
-      it(`can become a ${REGISTERED_ORGANIZATION} (registerOrganization)`, async () => {});
-      it(`can become a ${REGISTERED_GROUP} (registerGroup)`, async () => {});
-      it(`can become a ${REGISTERED_CUSTOM_GROUP} (registerCustomGroup)`, async () => {});
-      it(`only one path can be taken per ${msgSender}`, async () => {});
-      it(`only if ${msgSender} is not already registered`, async () => {});
+      it(`can become a ${REGISTERED_HUMAN} (registerHuman)`, async () => { });
+      it(`can become a ${REGISTERED_ORGANIZATION} (registerOrganization)`, async () => { });
+      it(`can become a ${REGISTERED_GROUP} (registerGroup)`, async () => { });
+      it(`can become a ${REGISTERED_CUSTOM_GROUP} (registerCustomGroup)`, async () => { });
+      it(`only one path can be taken per ${msgSender}`, async () => { });
+      it(`only if ${msgSender} is not already registered`, async () => { });
 
       describe(`${UNREGISTERED_AVATAR} to ${REGISTERED_HUMAN} (ON: registerHuman)`, () => {
-        it("only before REGISTRATION_PERIOD_END", async () => {});
-        it(`only if ${msgSender} has a token at the ${v1} hub`, async () => {});
-        it(`only if ${msgSender}'s ${v1} token is ${V1_STOPPED}`, async () => {});
-        it(`create a ${personalCirclesToken} for ${msgSender}`, async () => {});
+        it("only before REGISTRATION_PERIOD_END", async () => { });
+        it(`only if ${msgSender} has a token at the ${v1} hub`, async () => { });
+        it(`only if ${msgSender}'s ${v1} token is ${V1_STOPPED}`, async () => { });
+        it(`create a ${personalCirclesToken} for ${msgSender}`, async () => { });
       });
 
       // TODO: Are organizations or groups migrated from v1 to v2?
@@ -82,12 +82,12 @@ describe('Hub', () => {
       });
 
       describe(`${UNREGISTERED_AVATAR} to ${REGISTERED_GROUP} (ON: registerGroup)`, () => {
-        it(`must have the standard treasury contract`, async () => {});
+        it(`must have the standard treasury contract`, async () => { });
       });
 
       describe(`${UNREGISTERED_AVATAR} to ${REGISTERED_CUSTOM_GROUP} (ON: registerCustomGroup)`, () => {
-        it(`must have a treasury contract`, async () => {});
-        it(`can't have the standard treasury contract`, async () => {});
+        it(`must have a treasury contract`, async () => { });
+        it(`can't have the standard treasury contract`, async () => { });
       });
 
       describe(`${UNREGISTERED_AVATAR} to ${INVITED_HUMAN} (ON: inviteHuman)`, () => {
@@ -97,64 +97,65 @@ describe('Hub', () => {
         // When the INVITEE became an INVITED_HUMAN, the contract mints the INVITEE a welcome bonus consisting of the INVITED_HUMAN's personal Circles.
         // The INVITER is charged with the invitation fee.
         // TODO: Any avatar can invite any address (e.g. CREATE2 address with nothing deployed on yet)?.
-        it(`only if INVITER (${msgSender}) is a ${REGISTERED_HUMAN}`, async () => {});
-        it(`only if INVITEE is not already a ${REGISTERED_AVATAR}`, async () => {});
-        it(`only if INVITER (${msgSender}) has enough ${personalCirclesToken}s to pay the invitation fee`, async () => {});
-        it("mint INVITEE's welcome bonus", async () => {});
-        it(`charge INVITER (${msgSender}) the invitation fee`, async () => {});
-        it(`create a ${personalCirclesToken} for INVITEE`, async () => {});
+        it(`only if INVITER (${msgSender}) is a ${REGISTERED_HUMAN}`, async () => { });
+        it(`only if INVITEE is not already a ${REGISTERED_AVATAR}`, async () => { });
+        it(`only if INVITER (${msgSender}) has enough ${personalCirclesToken}s to pay the invitation fee`, async () => { });
+        it("mint INVITEE's welcome bonus", async () => { });
+        it(`charge INVITER (${msgSender}) the invitation fee`, async () => { });
+        it(`create a ${personalCirclesToken} for INVITEE`, async () => { });
 
         describe(`${UNREGISTERED_AVATAR} to ${INVITED_PAUSED_HUMAN} (ON: inviteHuman)`, () => {
-          it(`only if INVITEE's ${v1} token is ${V1_ACTIVE}`, async () => {});
-          it(`set INVITEE's ${personalCirclesToken}'s minting state to ${V2_PAUSED}`, async () => {});
+          it(`only if INVITEE's ${v1} token is ${V1_ACTIVE}`, async () => { });
+          it(`set INVITEE's ${personalCirclesToken}'s minting state to ${V2_PAUSED}`, async () => { });
         });
 
         describe(`${UNREGISTERED_AVATAR} to ${INVITED_ACTIVE_HUMAN} (ON: inviteHuman)`, () => {
-          it(`only if INVITEE's ${v1} token is ${V1_STOPPED}`, async () => {});
-          it(`set INVITEE's ${personalCirclesToken}'s minting state to ${V2_ACTIVE}`, async () => {});
+          it(`only if INVITEE's ${v1} token is ${V1_STOPPED}`, async () => { });
+          it(`set INVITEE's ${personalCirclesToken}'s minting state to ${V2_ACTIVE}`, async () => { });
         });
       });
     });
 
     describe(`(${REGISTERED_HUMAN} | ${INVITED_HUMAN}) to ${MINTED_PERSONAL_CIRCLES} (ON: personalMint)`, () => {
-      it(`only if ${msgSender}'s ${v1} ${personalCirclesToken} is ${V1_STOPPED}`, async () => {});
+      it(`only if ${msgSender} doesn't have a ${v1} token or it's ${V1_STOPPED}`, async () => { });
 
       describe(`${INVITED_PAUSED_HUMAN} to ${INVITED_ACTIVE_HUMAN} (ON: personalMint)`, () => {
         // If a UNREGISTERED_HUMAN was invited to v2 but still had n active v1 token then the minting state of the personal Circles token is V2_PAUSED.
         // If the INVITEE's v1 token was stopped in the meantime then the minting state of the personal Circles token will be set to V2_ACTIVE.
         // TODO: Should this be a separate function or do we integrate it into personalMint()?
-        it(`only if ${msgSender}'s ${personalCirclesToken}'s minting state is ${V2_PAUSED}`, async () => {});
-        it(`set ${msgSender}'s ${personalCirclesToken}'s minting state to ${V2_ACTIVE}`, async () => {});
+        it(`only if ${msgSender}'s ${personalCirclesToken} ${v1} minting state is ${V1_STOPPED}`, async () => { });
+        it(`only if ${msgSender}'s ${personalCirclesToken}'s minting state is ${V2_PAUSED}`, async () => { });
+        it(`set ${msgSender}'s ${personalCirclesToken}'s minting state to ${V2_ACTIVE}`, async () => { });
       });
 
       describe(`(${REGISTERED_HUMAN} | ${INVITED_ACTIVE_HUMAN}) to ${MINTED_PERSONAL_CIRCLES} (ON: personalMint)`, () => {
         // Every human with an active v2 token can mint max. two weeks worth of personal Circles tokens.
-        it(`only if ${msgSender}'s ${personalCirclesToken} is ${V2_ACTIVE}`, async () => {});
-        it(`mint as many ${personalCirclesToken}s as ${msgSender} has missed since the last mint`, async () => {});
-        it(`mint max. two weeks worth of ${personalCirclesToken} for ${msgSender}`, async () => {});
-        it(`set the lastMintTime of ${msgSender} to the current block time`, async () => {});
+        it(`only if ${msgSender}'s ${personalCirclesToken} is ${V2_ACTIVE}`, async () => { });
+        it(`mint as many ${personalCirclesToken}s as ${msgSender} has missed since the last mint`, async () => { });
+        it(`mint max. two weeks worth of ${personalCirclesToken} for ${msgSender}`, async () => { });
+        it(`set the lastMintTime of ${msgSender} to the current block time`, async () => { });
       });
     });
 
     describe(`Minting state of ${v1} Circles`, () => {
-      it(`can become ${V1_ACTIVE}`, async () => {});
-      it(`can become ${V1_STOPPED}`, async () => {});
-      it(`can't become ${V1_ACTIVE} again once ${V1_STOPPED}`, async () => {});
+      it(`can become ${V1_ACTIVE}`, async () => { });
+      it(`can become ${V1_STOPPED}`, async () => { });
+      it(`can't become ${V1_ACTIVE} again once ${V1_STOPPED}`, async () => { });
     });
 
     describe(`Minting state of ${v2} Circles`, () => {
-      it(`can become ${V2_PAUSED}`, async () => {});
-      it(`can become ${V2_ACTIVE}`, async () => {});
-      it(`can become ${V2_STOPPED}`, async () => {});
-      it(`can't become ${V2_PAUSED} again once ${V2_ACTIVE}`, async () => {});
-      it(`can't become ${V2_PAUSED} again once ${V2_STOPPED}`, async () => {});
-      it(`can't become ${V2_ACTIVE} again once ${V2_STOPPED}`, async () => {});
+      it(`can become ${V2_PAUSED}`, async () => { });
+      it(`can become ${V2_ACTIVE}`, async () => { });
+      it(`can become ${V2_STOPPED}`, async () => { });
+      it(`can't become ${V2_PAUSED} again once ${V2_ACTIVE}`, async () => { });
+      it(`can't become ${V2_PAUSED} again once ${V2_STOPPED}`, async () => { });
+      it(`can't become ${V2_ACTIVE} again once ${V2_STOPPED}`, async () => { });
     });
 
     describe(`${personalCirclesToken}s can be ${v1} or ${v2} tokens`, () => {
-      it(`a ${V2_PAUSED} token can coexist with an ${V1_ACTIVE} token for the same ${REGISTERED_AVATAR}`, async () => {});
-      it(`an ${V2_ACTIVE} token can coexist with a ${V1_STOPPED} token for a ${REGISTERED_AVATAR}`, async () => {});
-      it(`an ${V2_ACTIVE} token cannot coexist with an ${V1_ACTIVE} token for the same ${REGISTERED_AVATAR}`, async () => {});
+      it(`a ${V2_PAUSED} token can coexist with an ${V1_ACTIVE} token for the same ${REGISTERED_AVATAR}`, async () => { });
+      it(`an ${V2_ACTIVE} token can coexist with a ${V1_STOPPED} token for a ${REGISTERED_AVATAR}`, async () => { });
+      it(`an ${V2_ACTIVE} token cannot coexist with an ${V1_ACTIVE} token for the same ${REGISTERED_AVATAR}`, async () => { });
     });
   });
 });
