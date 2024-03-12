30428fdd28a85e8bc786c0f3dc9b655a819c578e
daniel <4954577+jaensen@users.noreply.github.com>
2024-03-13 00:11:23 +0100

    removed 'providers' package

--- packages/providers/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './browserWalletEthersProvider';
-export * from './provider';
-export * from './eoaEthersProvider';
--- packages/providers/src/provider.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from 'ethers';
-
-export interface Provider {
-  initialize: () => Promise<void>;
-  call: (args: { to: string, data: string }) => Promise<string>;
-  sendTransaction: (args: { to: string, data: string }) => Promise<ethers.TransactionResponse>;
-}
